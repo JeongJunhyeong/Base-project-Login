dc368e5cd1b02d11a829e4fbee29198479730290
JeongJunHyeong <kso6076@naver.com>
2025-04-04 04:06:52 +0900

    annotation

--- src/AuthService.java
+++ src/AuthService.java
@@ -6,17 +6,19 @@ import java.util.*;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
+// 인증(로그인 및 회원가입)을 처리하는 서비스 클래스
 public class AuthService {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String USERS_FILE = "users.json";
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    private Map<String, User> userMap = new HashMap<>();
+    private Map<String, User> userMap = new HashMap<>(); // ID를 키로 하는 사용자 정보 맵
 
     public AuthService() {
         loadUsersJson();
     }
 
+    // 메인 루프: 로그인, 회원가입, 종료 중 선택
     public void start() {
         while (true) {
             System.out.println("\n[1] 로그인  [2] 회원가입  [3] 종료");
@@ -41,6 +43,7 @@ public class AuthService {
         }
     }
 
+    // 로그인 처리 메소드
     private boolean login() {
         while (true) {
             System.out.print("ID 입력: ");
@@ -65,9 +68,11 @@ public class AuthService {
         }
     }
 
+    // 회원가입 처리 메소드
     private void signUp() {
-        String id, pw, nickname;
+        String id, passward, nickname;
 
+        // ID 입력 및 검증
         while (true) {
             System.out.print("ID (소문자 a-z, 4~12자): ");
             id = scanner.nextLine().replaceAll("\\s+", "");
@@ -85,11 +90,12 @@ public class AuthService {
             break;
         }
 
+        // 비밀번호 입력 및 검증
         while (true) {
             System.out.print("비밀번호 (8~20자, 대/소문자, 숫자, 특수문자 포함): ");
-            pw = scanner.nextLine().replaceAll("\\s+", "");
+            passward = scanner.nextLine().replaceAll("\\s+", "");
 
-            if (!isValidPassword(pw)) {
+            if (!isValidPassword(passward)) {
                 System.out.println("비밀번호 형식이 올바르지 않습니다. 다시 입력해주세요.");
                 continue;
             }
@@ -97,6 +103,7 @@ public class AuthService {
             break;
         }
 
+        // 닉네임 입력 및 중복 체크
         while (true) {
             System.out.print("닉네임 (영문/숫자/_.-, 4~12자): ");
             nickname = scanner.nextLine().replaceAll("\\s+", "");
@@ -123,26 +130,27 @@ public class AuthService {
             break;
         }
 
-        String hashedPw = hashPassword(pw);
+        String hashedPw = hashPassword(passward);
         User newUser = new User(nickname, hashedPw);
         userMap.put(id, newUser);
         saveUsersJson();
 
         System.out.println("회원가입이 완료되었습니다.");
     }
-
-    private boolean isValidPassword(String pw) {
-        return pw.length() >= 8 && pw.length() <= 20 &&
-                pw.matches(".*[a-z].*") &&
-                pw.matches(".*[A-Z].*") &&
-                pw.matches(".*[0-9].*") &&
-                pw.matches(".*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/].*");
+    // 비밀번호 유효성 검사 메소드
+    private boolean isValidPassword(String passward) {
+        return passward.length() >= 8 && passward.length() <= 20 &&
+                passward.matches(".*[a-z].*") &&
+                passward.matches(".*[A-Z].*") &&
+                passward.matches(".*[0-9].*") &&
+                passward.matches(".*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/].*");
     }
 
-    private String hashPassword(String pw) {
+    // SHA-256 해시 처리 메소드
+    private String hashPassword(String passward) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(pw.getBytes());
+            byte[] hash = md.digest(passward.getBytes());
             StringBuilder sb = new StringBuilder();
             for (byte b : hash) sb.append(String.format("%02x", b));
             return sb.toString();
@@ -151,6 +159,7 @@ public class AuthService {
         }
     }
 
+    // JSON 파일에서 사용자 정보 로드
     private void loadUsersJson() {
         try {
             if (Files.exists(Paths.get(USERS_FILE))) {
@@ -163,6 +172,7 @@ public class AuthService {
         }
     }
 
+    // 사용자 정보를 JSON 파일로 저장
     private void saveUsersJson() {
         try (Writer writer = Files.newBufferedWriter(Paths.get(USERS_FILE))) {
             gson.toJson(userMap, writer);
@@ -171,6 +181,7 @@ public class AuthService {
         }
     }
 
+    // 사용자 정보를 저장할 내부 클래스
     static class User {
         String nickname;
         String password;
